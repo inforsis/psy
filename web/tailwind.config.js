6fc9a2c49de02011a9f5b96da9cdf0c387e6c8b0
Jadson Moreira <inforsis@gmail.com>
2022-11-04 17:28:06 -0300

    npm install @mui/material @emotion/react @emotion/styled

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+],
   theme: {
     extend: {},
   },
